67ffa3007678460f007a2d9bacd2cde141b96c83
Reyan John Narvaja <73632471+reyanjoh@users.noreply.github.com>
2025-09-17 23:54:15 +0800

    updated brand color and theme index

--- components/theme/index.ts
+++ components/theme/index.ts
@@ -1,11 +1,15 @@
 import { createSystem, defaultConfig, defineConfig } from "@chakra-ui/react";
 import { colors } from "./colors";
+import { spacing } from "./spacing";
+import { recipes } from "./recipes";
 
 const config = defineConfig({
   theme: {
     tokens: {
       colors: colors,
+      spacing: spacing,
     },
+    recipes,
   },
 });
 
