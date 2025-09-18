d7861fd070a6706431d6147205fbf1b209482dad
Reyan John Narvaja <73632471+reyanjoh@users.noreply.github.com>
2025-09-19 03:08:41 +0800

    updated SideNav to use dynamic paths

--- layouts/layoutComponents/SideNavBar.tsx
+++ layouts/layoutComponents/SideNavBar.tsx
@@ -5,25 +5,26 @@ import { Box, Flex } from "@chakra-ui/react";
 import { Logo } from "@/components/icons/Logo";
 import { PowerIcon } from "@/components/icons/PowerIcon";
 import { SettingsIcon } from "@/components/icons/SettingsIcon";
+import { getSideNav } from "@/utils/navigation/getSideNav";
 import { MenuIcon } from "@/components/icons/MenuIcon";
 import { HomeIcon } from "@/components/icons/HomeIcon";
 import { TagsIcon } from "@/components/icons/TagsIcon";
 import { CartIcon } from "@/components/icons/CartIcon";
 import { ChatIcon } from "@/components/icons/ChatIcon";
+import Link from "next/link";
+import { useParams } from "next/navigation";
+import { usePathname } from "next/navigation";
 
 export const SideNavBar = () => {
   const [active, setActive] = useState<boolean>(false);
   const [settingsActive, setSettingsActive] = useState<boolean>(false);
+  const params = useParams();
+  const pathname = usePathname();
 
-  const [homeActive, setHomeActive] = useState<boolean>(false);
-  const [menuActive, setMenuActive] = useState<boolean>(false);
-  const [tagsActive, setTagsActive] = useState<boolean>(false);
-  const [cartActive, setCartActive] = useState<boolean>(false);
-  const [chatActive, setChatActive] = useState<boolean>(false);
+  const sideNav = getSideNav(params.employeeId as string);
 
   return (
     <Flex
-      // minH="100%"
       maxH={"calc(100vh - 7rem)"}
       bg="white"
       flexDir="column"
@@ -50,21 +51,31 @@ export const SideNavBar = () => {
           alignItems={"center"}
           justifyContent={"center"}
         >
-          <Box onClick={() => setHomeActive(!homeActive)}>
-            <HomeIcon active={homeActive} />
-          </Box>
-          <Box onClick={() => setMenuActive(!menuActive)}>
-            <MenuIcon active={menuActive} />
-          </Box>
-          <Box onClick={() => setTagsActive(!tagsActive)}>
-            <TagsIcon active={tagsActive} />
-          </Box>
-          <Box onClick={() => setCartActive(!cartActive)}>
-            <CartIcon active={cartActive} />
-          </Box>
-          <Box onClick={() => setChatActive(!chatActive)}>
-            <ChatIcon active={chatActive} />
-          </Box>
+          {sideNav.map((item) => (
+            <Box key={item.id}>
+              {item.icon === "HomeIcon" ? (
+                <Link href={item.path}>
+                  <HomeIcon active={pathname === item.path} />
+                </Link>
+              ) : item.icon === "MenuIcon" ? (
+                <Link href={item.path}>
+                  <MenuIcon active={pathname === item.path} />
+                </Link>
+              ) : item.icon === "TagsIcon" ? (
+                <Link href={item.path}>
+                  <TagsIcon active={pathname === item.path} />
+                </Link>
+              ) : item.icon === "CartIcon" ? (
+                <Link href={item.path}>
+                  <CartIcon active={pathname === item.path} />
+                </Link>
+              ) : item.icon === "ChatIcon" ? (
+                <Link href={item.path}>
+                  <ChatIcon active={pathname === item.path} />
+                </Link>
+              ) : null}
+            </Box>
+          ))}
         </Flex>
       </Flex>
 
