4817d5106d83649b599cb0b13c63e0f41b8c0117
reyanjoh <reyanjohnnarvaja@gmail.com>
2026-04-17 00:20:23 +0800

    removed other pages on navbar menu for simplisity

--- utils/navigation/getSideNav.ts
+++ utils/navigation/getSideNav.ts
@@ -1,34 +1,34 @@
 export function getSideNav(employeeId: string) {
   return [
-    {
-      id: 1,
-      name: "Home",
-      path: `/${employeeId}/home`,
-      icon: "HomeIcon",
-    },
+    // {
+    //   id: 1,
+    //   name: "Home",
+    //   path: `/${employeeId}/home`,
+    //   icon: "HomeIcon",
+    // },
     {
       id: 2,
       name: "Menu",
       path: `/${employeeId}/menu`,
       icon: "MenuIcon",
     },
-    {
-      id: 3,
-      name: "Saved Items",
-      path: `/${employeeId}/saved-items`,
-      icon: "TagsIcon",
-    },
+    // {
+    //   id: 3,
+    //   name: "Saved Items",
+    //   path: `/${employeeId}/saved-items`,
+    //   icon: "TagsIcon",
+    // },
     {
       id: 4,
       name: "Checkout",
       path: `/${employeeId}/checkout`,
       icon: "CartIcon",
     },
-    {
-      id: 5,
-      name: "Chat",
-      path: `/${employeeId}/chat`,
-      icon: "ChatIcon",
-    },
+    // {
+    //   id: 5,
+    //   name: "Chat",
+    //   path: `/${employeeId}/chat`,
+    //   icon: "ChatIcon",
+    // },
   ];
 }
