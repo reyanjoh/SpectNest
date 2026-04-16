5ff7e8b40b80bf9014776342dc7bc29d0fd35c22
reyanjoh <reyanjohnnarvaja@gmail.com>
2026-04-17 00:29:19 +0800

    removed unused code

--- app/[employeeId]/menu/Receipt.tsx
+++ app/[employeeId]/menu/Receipt.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React from "react";
 import { Card } from "@/components/Card";
 import { Flex, Text } from "@chakra-ui/react";
 import { useCartStore } from "../../../store/menu/cartStore";
