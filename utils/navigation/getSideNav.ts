c806366dc0ccfd67866a2bf7bbdcddcce1860444
reyanjoh <reyanjohnnarvaja@gmail.com>
2026-06-01 03:42:09 +0800

    new nav

--- utils/navigation/getSideNav.ts
+++ utils/navigation/getSideNav.ts
@@ -2,39 +2,39 @@ export function getSideNav(employeeId: string) {
   return [
     {
       id: 1,
-      name: "Home",
-      path: `/${employeeId}/home`,
+      name: "Dashboard",
+      path: `/home`,
       icon: "HomeIcon",
     },
     {
       id: 2,
-      name: "Menu",
-      path: `/${employeeId}/menu`,
+      name: "Products",
+      path: `/Products`,
       icon: "MenuIcon",
     },
     {
       id: 3,
-      name: "Saved Items",
-      path: `/${employeeId}/saved-items`,
-      icon: "TagsIcon",
+      name: "Inventory",
+      path: `/inventory`,
+      icon: "InventoryIcon",
     },
     {
       id: 4,
-      name: "Checkout",
-      path: `/${employeeId}/checkout`,
-      icon: "CartIcon",
+      name: "Reports",
+      path: `/reports`,
+      icon: "ReportsIcon",
     },
-    // {
-    //   id: 5,
-    //   name: "Chat",
-    //   path: `/${employeeId}/chat`,
-    //   icon: "ChatIcon",
-    // },
+    {
+      id: 5,
+      name: "Employees",
+      path: `/employees`,
+      icon: "EmployeesIcon",
+    },
     {
       id: 6,
-      name: "Inventory",
-      path: `/${employeeId}/inventory`,
-      icon: "InventoryIcon",
+      name: "Manage Store",
+      path: `/manage-store`,
+      icon: "ManageStoreIcon",
     },
   ];
 }
