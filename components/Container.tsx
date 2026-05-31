bc48e0c67b2ef4a6fb75e355127c4ba9aae4d6fe
reyanjoh <reyanjohnnarvaja@gmail.com>
2026-06-01 03:43:58 +0800

    added new container component

---
+++ components/Container.tsx
@@ -0,0 +1,38 @@
+import { Flex, FlexProps } from "@chakra-ui/react";
+import React from "react";
+
+type Props = FlexProps & {
+  children: React.ReactNode;
+  gap?: string;
+  w?: string;
+  bg?: string;
+  borderRadius?: string;
+  p?: string;
+  flexDirection?: string;
+};
+
+export const Container = ({
+  children,
+  w = "full",
+  bg = "white",
+  borderRadius = "md",
+  p = "1",
+  gap = "1",
+  flexDirection = "column",
+  ...rest
+}: Props) => {
+  return (
+    <Flex
+      w={w}
+      bg={bg}
+      borderRadius={borderRadius}
+      p={p}
+      pt={"1.5rem"}
+      gap={gap}
+      flexDirection={flexDirection}
+      {...rest}
+    >
+      {children}
+    </Flex>
+  );
+};
