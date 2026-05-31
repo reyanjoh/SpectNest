b96c33b4f7c6fc6e8d2f11b1008ecf004ca92d98
reyanjoh <reyanjohnnarvaja@gmail.com>
2026-06-01 01:24:54 +0800

    new icons and edited the old ones

--- components/icons/HomeIcon.tsx
+++ components/icons/HomeIcon.tsx
@@ -9,9 +9,9 @@ type Props = {
 };
 
 export const HomeIcon = ({
-  color = "#4D00F1",
-  height = "35",
-  width = "35",
+  color = "#5D6679",
+  height = "24",
+  width = "24",
   className,
   active = false,
 }: Props) => {
@@ -40,14 +40,14 @@ export const HomeIcon = ({
     >
       <path
         d="M9.02 2.83992L3.63 7.03992C2.73 7.73992 2 9.22992 2 10.3599V17.7699C2 20.0899 3.89 21.9899 6.21 21.9899H17.79C20.11 21.9899 22 20.0899 22 17.7799V10.4999C22 9.28992 21.19 7.73992 20.2 7.04992L14.02 2.71992C12.62 1.73992 10.37 1.78992 9.02 2.83992Z"
-        stroke="#171717"
+        stroke={color}
         strokeWidth="2"
         strokeLinecap="round"
         strokeLinejoin="round"
       />
       <path
         d="M12 17.99V14.99"
-        stroke="#171717"
+        stroke={color}
         strokeWidth="2"
         strokeLinecap="round"
         strokeLinejoin="round"
