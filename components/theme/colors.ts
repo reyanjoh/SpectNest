7f6cd5b16aed0c134811d6ecfef909ce1032bed7
reyanjoh <reyanjohnnarvaja@gmail.com>
2026-05-31 22:11:27 +0800

    added colors

--- components/theme/colors.ts
+++ components/theme/colors.ts
@@ -1,4 +1,14 @@
 export const colors = {
   brand: { value: "#4D00F1" },
+  brandVariant: {
+    1: { value: "#009ED8" },
+  },
+  gray: {
+    border: { value: "#D0D5DD" },
+    bg: { value: "#EFEFEF" },
+  },
   bgGray: { value: "#EFEFEF" },
+  fontGray: {
+    1: { value: "#667085" },
+  },
 };
